2798912a70c52d971f1e5446bd7c2cedfb815e78
minipele06 <aja398@stern.nyu.edu>
2020-06-13 14:34:34 -0400

    cleaning up files

--- advanced_cart.py
+++ advanced_cart.py
@@ -30,7 +30,7 @@ print("Once completed, enter 'Done' to proceed.")
 while True:
     try:
         response = input("Please Enter Product ID: ")
-        if response == "Done":
+        if response.capitalize() == "Done":
             break
         else:
             response = int(response)
